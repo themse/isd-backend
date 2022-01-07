0d5160df4cef058013313b5a24b38117af988bb9
Den Polusmyak <den.polusmyak@gmail.com>
2022-01-08 01:04:03 +0200

    formatting

--- src/functions/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as createLead } from './leads/create-lead';
-export { default as findLeads } from './leads/find-leads';
-export { default as createInterest } from './interests/create-interest';
